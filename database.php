03efb23cc3cb63a6db03c5da433544abc9503a28
Abdullah Al Mammoon <mamoon036@gmail.com>
2020-08-27 12:30:11 +0600

    add database creation file

---
+++ database.php
@@ -0,0 +1,48 @@
+<?php
+$con = mysqli_connect('localhost','root','','test');
+$sql = "CREATE TABLE brands(
+    brandId int(10) NOT NULL AUTO_INCREMENT,
+    brandName varchar(50),
+    PRIMARY KEY(brandId)
+)";
+
+
+if(mysqli_query($con,$sql)){
+    echo "ok";
+} else{
+    echo "not ok";
+}
+
+$sql1 = "CREATE TABLE categoris(
+    catId int(10) NOT NULL AUTO_INCREMENT,
+    catName varchar(50),
+    image varchar(50),
+    PRIMARY KEY(catId)
+)";
+
+mysqli_query($con,$sql1);
+
+$sql2 = "CREATE TABLE orders(
+    id int(10) NOT NULL AUTO_INCREMENT,
+    product_id varchar(50),
+    quantity varchar(50),
+    customerName varchar(150),
+    address varchar(150),
+    phone varchar(50),
+    PRIMARY KEY(id)
+)";
+
+mysqli_query($con,$sql2);
+
+$sql3 = "CREATE TABLE products(
+    productId int(10) NOT NULL AUTO_INCREMENT,
+    brand varchar(50),
+    model varchar(50),
+    categoryId varchar(150),
+    address varchar(150),
+    price varchar(50),
+    offer varchar(50),
+    PRIMARY KEY(productId)
+)";
+
+mysqli_query($con,$sql3);
